6dff67225a9294eb260325253e6c28d6f7b12306
Oscar Perez <100051211+Astro024@users.noreply.github.com>
2024-10-10 00:42:19 +0000

    done

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import random
 
 def main():
     total = 0
-    numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    numbers = [random.randint(0,100) for _ in range(5)]
+    total = sum(numbers[:-1])
+    print(total)
+
+    #i = 0
+    
+   
+
+    
+
+
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
